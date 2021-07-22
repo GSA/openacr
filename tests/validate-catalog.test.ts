6ef66f854a2ded1dfb433d79502a3a027c12a43a
Daniel Mundra <daniel.mundra@civicactions.com>
2021-07-22 08:57:12 -0700

    Updated catalog validation tests.

--- tests/validate-catalog.test.ts
+++ tests/validate-catalog.test.ts
@@ -60,13 +60,42 @@ describe("Validate catalog", () => {
     ],
   };
   const invalidJSON = { foo: 2, bar: 4 };
+  const invalidJSON1 = {
+    title: "WCAG 2.0",
+    chapters: {
+      id: "success_criteria_level_a",
+      label: "Success Criteria, Level A",
+    },
+  };
+  const invalidJSON2 = {
+    title: "WCAG 2.0",
+    chapters: [
+      {
+        id: "success_criteria_level_a",
+        label: "Success Criteria, Level A",
+        criteria: ["1.1.1", "1.2.2"],
+      },
+    ],
+  };
+  const invalidJSON3 = {
+    title: "WCAG 2.0",
+    components: [
+      {
+        web: "Web",
+      },
+      {
+        "electronic-docs": "Electronic Docs",
+      },
+    ],
+  };
   let result = null;
 
   it("pass valid JSON with valid schema should return valid message", () => {
     result = validateCatalog(validJSON, validSchema);
-    //expect(result.result).to.equal(true);
+    expect(result.result).to.equal(true);
     expect(result.message).to.equal("Valid!");
   });
+
   it("pass invalid JSON with valid schema should return invalid JSON message", () => {
     result = validateCatalog(invalidJSON, validSchema);
     expect(result.result).to.equal(false);
@@ -74,14 +103,41 @@ describe("Validate catalog", () => {
       "Invalid: data must have required property 'title'"
     );
   });
+
   it("pass valid JSON with invalid schema should return invalid schema message", () => {
     result = validateCatalog(validJSON, invalidSchema);
     expect(result.result).to.equal(false);
     expect(result.message).to.equal("Invalid: schema is not valid");
   });
+
   it("pass invalid JSON with invalid schema should return invalid schema message", () => {
     result = validateCatalog(invalidJSON, invalidSchema);
     expect(result.result).to.equal(false);
     expect(result.message).to.equal("Invalid: schema is not valid");
+  });
+
+  it("pass invalid chapters JSON should return invalid JSON message", () => {
+    result = validateCatalog(invalidJSON1, validSchema);
+    expect(result.result).to.equal(false);
+    expect(result.message).to.equal("Invalid: data/chapters must be array");
+  });
+
+  it("pass invalid criteria JSON should return invalid JSON message", () => {
+    result = validateCatalog(invalidJSON2, validSchema);
+    expect(result.result).to.equal(false);
+    expect(result.message).to.equal(
+      "Invalid: data/chapters/0/criteria/0 must be object, " +
+        "data/chapters/0/criteria/1 must be object"
+    );
+  });
+
+  it("pass invalid components JSON should return invalid JSON message", () => {
+    result = validateCatalog(invalidJSON3, validSchema);
+    expect(result.result).to.equal(false);
+    expect(result.message).to.equal(
+      "Invalid: data/components/0 must have required property 'id', data/components/0 must have required property 'label', " +
+        "data/components/1 must have required property 'id', " +
+        "data/components/1 must have required property 'label'"
+    );
   });
 });
