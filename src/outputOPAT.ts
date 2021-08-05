7d19e5b68dd8bb53f7003f1f40870adf37a34977
Daniel Mundra <daniel.mundra@civicactions.com>
2021-08-05 08:58:48 -0700

    Adjusted handling of none label. Added test that generates drupal-9 markdown and updated OPAT.

--- src/outputOPAT.ts
+++ src/outputOPAT.ts
@@ -43,7 +43,9 @@ export function outputOPAT(
       if (catalogData.components) {
         for (const component of catalogData.components) {
           if (component.id === componentId) {
-            return component.label;
+            if (component.label != "") {
+              return "**" + component.label + "**: ";
+            }
           }
         }
       }
@@ -57,6 +59,7 @@ export function outputOPAT(
           }
         }
       }
+      return "Not Applicable";
     });
 
     const result = template(data);
