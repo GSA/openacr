b5e976b4692b47726ee77f2152f9040d45725030
Daniel Mundra <daniel.mundra@civicactions.com>
2021-08-30 08:23:02 -0700

    Fixed minor issue in govready yaml. Regenerated outputs. Added it to reports and updated docs.

--- opat/reports.js
+++ opat/reports.js
@@ -7,6 +7,14 @@ const dataSet = [
     "<a href='drupal-9.yaml'>OPAT</a>",
     "<a href='../catalog/2.4-edition-wcag-2.0-508-en.yaml'>VPAT® 2.4 Revised Section 508 Edition</a>",
   ],
+  [
+    "<a href='govready-0.9.html'>GovReady PBC Accessibility Conformance Report</a>",
+    "GovReady 0.9.1.36",
+    "GovReady is a user-friendly cybersecurity Governance, Risk, and Compliance (GRC) web application focused on guiding teams through the NIST Risk Management Framework using Compliance-As-Code and collaborative features.",
+    "<a href='govready-0.9.markdown'>Markdown</a>",
+    "<a href='govready-0.9.yaml'>OPAT</a>",
+    "<a href='../catalog/2.4-edition-wcag-2.0-508-en.yaml'>VPAT® 2.4 Revised Section 508 Edition</a>",
+  ],
 ];
 
 $(document).ready(function () {
